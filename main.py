a4c5a97ce667190cc9e3439f3ea0f8c4fbe47167
chalftree <matej.krizek022@gmail.com>
2025-06-02 09:00:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-import tkinter
 import random
+
+def gamble(money):
+    gambling = random.randint(1,100)
+    if gambling == 1:
+        winprompt = 1
+    if gambling != 1:
+        winprompt = 0
+    return winprompt
+    
+
+money = 1000
+winprompt = 0
+
+action = input()
+
+if action == "gamble":
+    output = gamble(money)
+    if output == 1:
+        print("vyhrar")
+        money = money + 10000
+        print(money)
+    if output == 0:
+        print("prohrar")
+        money = money - 20
+        print(money)
